07598230c3c7a5832f1c28fcdf53e20c9596bf2f
Lud0do1202 <ludo.traina@gmail.com>
2023-10-09 14:12:39 +0200

    menu-y custom

--- src/app/pages/components/menu/menu-y-icons/menu-y-icons-page.component.ts
+++ src/app/pages/components/menu/menu-y-icons/menu-y-icons-page.component.ts
@@ -37,7 +37,7 @@ export class DemoMenuXComponent {
   loaded: number = 0;
 }`;
 
-  // Default
+  /* -------------------------------- Default ------------------------------- */
   defaultLoaded: number = 3;
   defaultSelect: number = 0;
   defaultHtml: string = `<gems-menu-y-icons [links]="links" (onLinkClicked)="loaded = $event"></gems-menu-y-icons>
@@ -56,5 +56,87 @@ export class DemoMenuXComponent {
 
   // Default component to load
   loaded: number = 3;
+}`;
+
+  /* --------------------------------- Custom --------------------------------- */
+  customLoaded: number = 0;
+  customId: string = 'custom-menu-y-icons';
+  customSelect: number = 0;
+  customHtml: string = `<gems-menu-y-icons [styleId]="'custom-menu-y-icons'" [links]="links" (onLinkClicked)="loaded = $event"></gems-menu-y-icons>
+
+<div style="text-align: center; margin-left: 1rem">
+  <div *ngIf="loaded === 0">Component 'History' loaded</div>
+  <div *ngIf="loaded === 1">Component 'User' loaded</div>
+  <div *ngIf="loaded === 2">Component 'Calendar' loaded</div>
+  <div *ngIf="loaded === 3">Component 'Chart' loaded</div>
+</div>
+</div>`;
+  customTypescript: string = `import { MenuYIconsModule } from '@lud0do1202/angular-gems';
+
+export class DemoMenuXComponent {
+  // Labels of the links
+  links: string[] = ['History', 'User', 'Calendar', 'Chart'];
+
+  // The component to load (must be 0 if a default active link is not set)
+  loaded: number = 0;
+}`;
+  customScss: string = `// <nav> : menu-y-icons
+:host ::ng-deep #custom-menu-y-icons {
+  &.gems-menu-y-icons {
+    background-color: #d4b254;
+
+    // <ul> : list of the links
+    .gems-list {
+      // <li> : one item of the list
+      .gems-item {
+        // <li class="gems-electron-item"> : the item which move
+        &.gems-electron-item {
+          .gems-link {
+            background-color: #d47522;
+          }
+        }
+
+        // <li class="active"> : The item which is related to the active link
+        &.active {
+          .gems-link {
+            color: #000;
+          }
+        }
+
+        // <a> : The link of an item
+        .gems-link {
+          color: #8e0391;
+          transition: 0.5s;
+
+          &:hover {
+            cursor: crosshair;
+            border: 1px solid #5662e1;
+            color: #8e039155;
+          }
+        }
+      }
+
+      // Second item of the menu
+      .gems-item-1 {
+        &.gems-electron-item {
+          .gems-link {
+            background-color: rgb(74, 230, 62);
+          }
+        }
+
+        &.active {
+          .gems-link {
+            color: rgb(200, 240, 119);
+            background-color: transparent;
+          }
+        }
+
+        .gems-link {
+          background-color: rgb(213, 22, 53);
+          color: rgb(3, 103, 243);
+        }
+      }
+    }
+  }
 }`;
 }
